a9c8c8e1cf1fa32499b32805ccdae536e345a274
Jason <jsondotbower@users.noreply.github.com>
2017-10-06 23:04:25 -0500

    Format changes
    Updated format to both Main.java apps

--- GrossProfitCalculator/src/com/company/Main.java
+++ GrossProfitCalculator/src/com/company/Main.java
@@ -1,25 +1,22 @@
 package com.company;
+
 import java.util.Scanner;
 
-public class Main
-{
+public class Main {
 
     // Function to calculate Gross Profit
-    public static double grossProfitCalculator(double cost, double retail)
-    {
+    public static double grossProfitCalculator(double cost, double retail) {
         double profit = retail - cost;
         return profit;
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         // Variables
         double cost = 0, retailPrice = 0;
         String error = "Please enter a valid product price and product cost.";
         Scanner keyboard = new Scanner(System.in);
 
-        try
-        {
+        try {
             // Get user input for retailPrice
             System.out.print("Enter your product price: ");
             retailPrice = Double.parseDouble(keyboard.next());
@@ -27,9 +24,7 @@ public class Main
             // Get user input for cost
             System.out.print("Enter your product cost: ");
             cost = Double.parseDouble(keyboard.next());
-        }
-        catch (NumberFormatException bad)
-        {
+        } catch (NumberFormatException bad) {
             System.out.println(error);
             System.exit(1);
         }
