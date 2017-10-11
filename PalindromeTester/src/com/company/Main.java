b9cf1dcf0817791eea83c3adfbbec04575eda2e2
Jason <jsondotbower@users.noreply.github.com>
2017-10-10 22:51:58 -0500

    Update to PalindromeTester

--- PalindromeTester/src/com/company/Main.java
+++ PalindromeTester/src/com/company/Main.java
@@ -4,36 +4,9 @@ import java.util.Scanner;
 
 public class Main {
     // Function to determine String value is a palindrome
-    public static boolean isPalindrome(String value) {
-        int min = 0;
-        int max = value.length() - 1;
-
-        while (true) {
-            if (min > max) {
-                return true;
-            }
-
-            // Check for letters from left to right
-            while (!Character.isLetter(value.charAt(min))) {
-                min++;
-            }
-
-            // Check for letters from right to left
-            while (!Character.isLetter(value.charAt(max))) {
-                max--;
-            }
-
-            char a = Character.toLowerCase(value.charAt(min));
-            char b = Character.toLowerCase(value.charAt(max));
-
-            if (a != b) {
-                // Returns false if letters are not equal
-                return false;
-            }
-
-            min++;
-            max--;
-        }
+    public static boolean isPalindrome(String str) {
+        StringBuilder sb = new StringBuilder(str);
+        return str.equals(sb.reverse().toString());
     }
 
     public static void main(String[] args) {
